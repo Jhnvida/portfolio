19fa453c6e4508875f55e05fe5a00b190a34aae1
Jhnvida <joao.vida.andre@gmail.com>
2026-09-16 22:23:10 -0300

    refactor: ajusta espacamento no split text para evitar corte de texto

--- src/components/ui/SplitText.tsx
+++ src/components/ui/SplitText.tsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+import React from "react";
 import { cn } from "../../lib/utils";
 
 type SplitTextProps = {
@@ -14,11 +14,14 @@ export function SplitText({ text, className, wordClassName }: SplitTextProps) {
         <span className={cn("inline-block", className)} aria-label={text}>
             {words.map((word, wordIndex) => (
                 <span key={wordIndex} className="inline-block whitespace-nowrap" aria-hidden="true">
-                    <span className="inline-block overflow-hidden py-1">
-                        <span className={cn("inline-block split-word will-change-transform", wordClassName)}>
+                    <span className="inline-block overflow-hidden pb-[0.3em] mb-[-0.3em] pt-[0.1em] mt-[-0.1em]">
+                        <span
+                            className={cn("inline-block split-word will-change-transform leading-tight", wordClassName)}
+                        >
                             {word}
                         </span>
                     </span>
+
                     {wordIndex < words.length - 1 && <span className="inline-block">&nbsp;</span>}
                 </span>
             ))}
